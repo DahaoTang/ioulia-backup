15bdf90a5650773aabee556c60398d6eb47847d4
Dahao Tang <dahaotang@gmail.com>
2023-12-05 02:56:46 +1100

    landing page init

---
+++ app/(site)/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Heading1 } from "@/app/components/Headings";
+
+export default function Home() {
+	return (
+		<div className="w-full h-full flex flex-col bg-gradient-to-br from-emerald-300 from-10% via-blue-300 via-30% to-fuchsia-300 to-90%">
+			<div className="pl-1">
+				<Heading1 className="text-sky-600">ioulia</Heading1>
+				<div className="pt-1 text-sky-800">
+					Your personal AI assistant close as a friend.
+				</div>
+			</div>
+			<div className="h-screen flex flex-col justify-center items-center bg-rose-300">
+				<Heading1 className="text-sky-600 pb-20">Coming Soon</Heading1>
+			</div>
+			<div className="h-auto flex justify-center items-center bg-rose-300">
+				<div className="pb-1 text-neutral-500">
+					Developed by{" "}
+					<a
+						className="underline"
+						href="https://dahaotang.com/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Dahao Tang
+					</a>
+					.&nbsp;2023
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ app/components/Headings.tsx
@@ -0,0 +1,42 @@
+type HeadingProps = {
+	children: string;
+	className?: string;
+};
+
+const Heading1: React.FC<HeadingProps> = ({ children, className }) => {
+	return (
+		<div className={`text-5xl font-bold  ${className || ""}`}>{children}</div>
+	);
+};
+
+const Heading2: React.FC<HeadingProps> = ({ children, className }) => {
+	return (
+		<div className={`text-4xl font-bold  ${className || ""}`}>{children}</div>
+	);
+};
+
+const Heading3: React.FC<HeadingProps> = ({ children, className }) => {
+	return (
+		<div className={`text-3xl font-bold  ${className || ""}`}>{children}</div>
+	);
+};
+
+const Heading4: React.FC<HeadingProps> = ({ children, className }) => {
+	return (
+		<div className={`text-2xl font-bold  ${className || ""}`}>{children}</div>
+	);
+};
+
+const Heading5: React.FC<HeadingProps> = ({ children, className }) => {
+	return (
+		<div className={`text-xl font-bold  ${className || ""}`}>{children}</div>
+	);
+};
+
+const Heading6: React.FC<HeadingProps> = ({ children, className }) => {
+	return (
+		<div className={`text-l font-bold  ${className || ""}`}>{children}</div>
+	);
+};
+
+export { Heading1, Heading2, Heading3, Heading4, Heading5, Heading6 };
